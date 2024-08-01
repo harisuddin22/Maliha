ce7deee64deba02db0d1f12f73f3698735cdbac7
harisuddin22 <160148739+harisuddin22@users.noreply.github.com>
2024-08-01 21:41:33 +0500

    Started New Changes

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
+
+// For Product Modal
