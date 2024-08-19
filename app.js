11e87f1ad1bb36fc6998d711e1150a211b422fb4
harisuddin22 <160148739+harisuddin22@users.noreply.github.com>
2024-08-19 12:06:12 +0500

    new changes

--- app.js
+++ app.js
@@ -7,4 +7,48 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// For Product Modal
+// For Products
+
+$('.buy').click(function(){
+    $('.bottom').addClass("clicked");
+  });
+  
+  $('.remove').click(function(){
+    $('.bottom').removeClass("clicked");
+  });
+
+
+  let black = document.getElementById("black")
+  let red = document.getElementById("red")
+  let orange = document.getElementById("orange")
+
+
+    black.addEventListener("click", ()=>{
+        black.classList.add("active")
+        red.classList.remove("active");
+        orange.classList.remove("active")
+
+});
+
+    red.addEventListener("click", ()=>{
+        red.classList.add("active")
+        black.classList.remove("active");
+        orange.classList.remove("active")
+
+});
+
+    orange.addEventListener("click", ()=>{
+        orange.classList.add("active")
+        red.classList.remove("active");
+        black.classList.remove("active");
+
+
+});
+
+
+var cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e)=>{
+    cursor.style.top = (e.pageY-cursor.offsetHeight/2+"px")
+    cursor.style.left = (e.pageX-cursor.offsetWidth/2+"px")
+})
